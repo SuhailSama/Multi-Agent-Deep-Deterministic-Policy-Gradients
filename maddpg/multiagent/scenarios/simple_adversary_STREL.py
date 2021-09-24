becb568504daeccc1b872a7203a199654f75c308
Suhail Alsalehi <33728090+SuhailSama@users.noreply.github.com>
2021-09-24 10:24:23 -0400

    Update simple_adversary_STREL.py

--- maddpg/multiagent/scenarios/simple_adversary_STREL.py
+++ maddpg/multiagent/scenarios/simple_adversary_STREL.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
@@ -9,25 +10,26 @@ class Scenario(BaseScenario):
         world = World()
         # set any world properties first
         world.dim_c = 2 # communication channel dimensionality
-        num_agents = 3
+        num_agents = 5
         world.num_agents = num_agents
-        num_adversaries = 1
-        num_landmarks = num_agents - num_adversaries
+        num_adversaries = 2
+        num_landmarks = 1 # num_agents - num_adversaries
+        execution_time = 20
         # add agents
         world.agents = [Agent() for i in range(num_agents)]
         for i, agent in enumerate(world.agents):
             agent.name = 'agent %d' % i
-            agent.collide = False
+            agent.collide = True
             agent.silent = True
             agent.adversary = True if i < num_adversaries else False
-            agent.size = 0.15
+            agent.size = 0.10 if i < num_adversaries else 0.15
         # add landmarks
         world.landmarks = [Landmark() for i in range(num_landmarks)]
         for i, landmark in enumerate(world.landmarks):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
             landmark.movable = False
-            landmark.size = 0.08
+            landmark.size = 0.05
         # make initial conditions
         self.reset_world(world)
         return world
@@ -39,17 +41,27 @@ class Scenario(BaseScenario):
             world.agents[i].color = np.array([0.35, 0.35, 0.85])
         # random properties for landmarks
         for i, landmark in enumerate(world.landmarks):
-            landmark.color = np.array([0.15, 0.15, 0.15])
+            landmark.color = np.array([1, 0.15, 0.15])
         # set goal landmark
         goal = np.random.choice(world.landmarks)
-        goal.color = np.array([0.15, 0.65, 0.15])
+        goal.color = np.array([0.35, 0.35, 0.85])
+        
+        sheep = np.random.choice(world.landmarks)
+        while sheep == goal: 
+            sheep = np.random.choice(world.landmarks)
+        sheep.color = np.array([0.15, 0.15, 0.15])
+        
         for agent in world.agents:
             agent.goal_a = goal
+            
         # set random initial states
         for agent in world.agents:
             agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.p_pos_series = np.random.uniform(-1, +1, world.dim_p)
+            agent.state.p_vel_series = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c) # communication noise
+            
         for i, landmark in enumerate(world.landmarks):
             landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
@@ -74,76 +86,54 @@ class Scenario(BaseScenario):
         return [agent for agent in world.agents if agent.adversary]
 
     def reward(self, agent, world):
-        # Agents are rewarded based on minimum agent distance to each landmark
-        return self.adversary_reward(agent, world) if agent.adversary else self.agent_reward(agent, world)
-
-    def agent_reward(self, agent, world):
-        # Rewarded based on how close any good agent is to the goal landmark, and how far the adversary is from it
-        shaped_reward = True
-        shaped_adv_reward = True
-
-        # Calculate negative reward for adversary
-        adversary_agents = self.adversaries(world)
-        if shaped_adv_reward:  # distance-based adversary reward
-        
-            adv_rew = sum([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in adversary_agents])
-        else:  # proximity-based adversary reward (binary)
-            adv_rew = 0
-            for a in adversary_agents:
-                if np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) < 2 * a.goal_a.size:
-                    adv_rew -= 5
-
-        # Calculate positive reward for agents
+        # Agents are rewarded based on STREL robustness score
         good_agents = self.good_agents(world)
-        if shaped_reward:  # distance-based agent reward
-            pos_rew = -min(
-                [np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in good_agents])
-        else:  # proximity-based agent reward (binary)
-            pos_rew = 0
-            if min([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in good_agents]) \
-                    < 2 * agent.goal_a.size:
-                pos_rew += 5
-            pos_rew -= min(
-                [np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in good_agents])
-        return self.Robustness(agent, world)        
-        #return pos_rew + adv_rew
-        
-    def Robustness(self, agent, world):
+        result = self.adversary_reward(agent, world) if agent.adversary else np.sum([self.agent_reward(a, world) for a in good_agents])/len(good_agents)
+        #print(agent.name, ', adv? ',agent.adversary, ', reward = ',result)
+        return result
+
         # robustness formula
-        # \phi = good_agents reach_{r<min_dist} landmark AND ~ (adversary_agents reach_{r<min_dist} landmark)
-        min_gd2gl_dist = 8 * agent.goal_a.size
-        min_gd2ad_dist = 2 * agent.goal_a.size
-        min_gd2gd_dist = 18 * agent.goal_a.size
+        # \phi = Eventually_{[5,10]} good_agents surround_{r<min_gd2ad_dist} landmark 
+        #       AND Eventually_{[5,10]} sheep reach_{r<min_dist} goal
+        #       AND Always_{[0,20]} ~(adversary_agents reach_{r<min_dist} landmark)
+        
+        
+    def agent_reward(self, agent, world):
+
+        min_gd2gl_dist = 1.5 * agent.goal_a.size
+        max_gd2gl_dist = 3 * agent.goal_a.size
+        min_gl2ad_dist = agent.size + 2 * agent.goal_a.size
+        min_gd2gd_dist = 1.5 * agent.size
+        max_gd2gd_dist = 1.5 * agent.size
+        
         # Calculate distance to goal from adversary_agents
         adversary_agents = self.adversaries(world)
-        adv2goal_dist = sum([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in adversary_agents])
+        adv2gl_dist = np.mean([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in adversary_agents])
 
-        # Calculate distance from good_agents to goal 
-        good_agents = self.good_agents(world)
-        gd2goal_dist = -min([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in good_agents])
+        # Calculate distance from agent to goal 
+        gd2gl_dist = np.sqrt(np.sum(np.square(agent.state.p_pos - agent.goal_a.state.p_pos)))
+        #gd2gl_dist = max([np.sqrt(np.sum(np.square(a.state.p_pos - a.goal_a.state.p_pos))) for a in good_agents])
         
-        # Calculate distance from good_agents to goal 
+        # Calculate minimum distance between good_agents
         gd2gd_dist_temp = []
         good_agents = self.good_agents(world)
         n_good_agents = len(good_agents)
-        ind = 0
         for a in good_agents:
-            for i in range(ind+1,n_good_agents,1):
-                gd2gd_dist_temp.append( np.sqrt(np.sum(np.square(a.state.p_pos - good_agents[i].state.p_pos))) )
-            if ind+1 < n_good_agents:
-                ind =+ 1
-            else: 
-                break
+            if a.name != agent.name:
+                gd2gd_dist_temp.append( np.sqrt(np.sum(np.square(a.state.p_pos - agent.state.p_pos))) )
+
         gd2gd_dist = min(gd2gd_dist_temp)
+        #rob_score = min([ (gd2gl_dist - min_gd2gl_dist)/gd2gl_dist, (max_gd2gl_dist - gd2gl_dist)/gd2gl_dist, 2*(adv2gl_dist - min_gl2ad_dist)/(adv2gl_dist+min_gl2ad_dist),
+        #                  (gd2gd_dist - min_gd2gd_dist)/gd2gd_dist, (max_gd2gd_dist - gd2gd_dist)/gd2gd_dist])
+        rob_score = np.mean([ (max_gd2gl_dist - gd2gl_dist), (adv2gl_dist - min_gl2ad_dist),(max_gd2gd_dist - gd2gd_dist)])
         
-        rob_score = min([min_gd2gl_dist-gd2goal_dist, adv2goal_dist-min_gd2ad_dist, min_gd2gd_dist-gd2gd_dist])
         return rob_score
         
     def adversary_reward(self, agent, world):
         # Rewarded based on proximity to the goal landmark
         shaped_reward = True
         if shaped_reward:  # distance-based reward
-            return -np.sum(np.square(agent.state.p_pos - agent.goal_a.state.p_pos))
+            return -np.sqrt(np.sum(np.square(agent.state.p_pos - agent.goal_a.state.p_pos)))
         else:  # proximity-based reward (binary)
             adv_rew = 0
             if np.sqrt(np.sum(np.square(agent.state.p_pos - agent.goal_a.state.p_pos))) < 2 * agent.goal_a.size:
@@ -169,5 +159,3 @@ class Scenario(BaseScenario):
             return np.concatenate([agent.goal_a.state.p_pos - agent.state.p_pos] + entity_pos + other_pos)
         else:
             return np.concatenate(entity_pos + other_pos)
-
-
